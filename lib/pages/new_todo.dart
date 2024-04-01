d5a92caec0692bdcbd22cb98fe6dba7b66bcd300
Hoang Truong <xuantruongit32@gmail.com>
2024-04-01 10:29:33 +0700

    feat: add showdatepicker and showtimepicker

--- lib/pages/new_todo.dart
+++ lib/pages/new_todo.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 
 class NewTodo extends StatefulWidget {
@@ -9,12 +11,36 @@ class NewTodo extends StatefulWidget {
 }
 
 class _NewTodoState extends State<NewTodo> {
+  DateTime selectedTimeBegin = DateTime.now();
+  DateTime selectedTimeEnd = DateTime.now().add(
+    const Duration(hours: 1),
+  );
+  final _titleController = TextEditingController();
+  Future<void> _selectDate(bool beginOrEnd) async {
+    var pick = await showDatePicker(context: context, firstDate: DateTime(2000), lastDate: DateTime(2100));
+    if (pick != null) {
+      setState(() {
+        beginOrEnd ? selectedTimeBegin = pick : selectedTimeEnd = pick;
+      });
+    }
+  }
+
+  Future<void> _selectTime(bool beginOrEnd) async {
+    var pick = await showTimePicker(
+        context: context, initialTime: TimeOfDay.fromDateTime(beginOrEnd ? selectedTimeBegin : selectedTimeEnd));
+    if (pick != null) {
+      setState(() {
+        beginOrEnd
+            ? selectedTimeBegin =
+                DateTime(selectedTimeBegin.year, selectedTimeBegin.month, selectedTimeBegin.day, pick.hour, pick.minute)
+            : selectedTimeEnd =
+                DateTime(selectedTimeEnd.year, selectedTimeEnd.month, selectedTimeEnd.day, pick.hour, pick.minute);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    DateTime _selectedTimeBegin = DateTime.now();
-    DateTime _selectedTimeEnd = DateTime.now().add(
-      const Duration(hours: 1),
-    );
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -29,8 +55,9 @@ class _NewTodoState extends State<NewTodo> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const TextField(
-              decoration: InputDecoration(
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(
                 hintText: 'Add Title',
                 hintStyle: TextStyle(
                   fontSize: 36,
@@ -38,7 +65,7 @@ class _NewTodoState extends State<NewTodo> {
                 alignLabelWithHint: true,
                 border: InputBorder.none,
               ),
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 36,
               ),
             ),
@@ -48,14 +75,24 @@ class _NewTodoState extends State<NewTodo> {
             const Divider(),
             Row(
               children: [
-                Text(
-                  DateFormat('E, MMM dd, yyyy').format(_selectedTimeBegin),
-                  style: const TextStyle(fontSize: 30),
+                GestureDetector(
+                  onTap: () {
+                    _selectDate(true);
+                  },
+                  child: Text(
+                    DateFormat('E, MMM dd, yyyy').format(selectedTimeBegin),
+                    style: const TextStyle(fontSize: 30),
+                  ),
                 ),
                 const SizedBox(width: 50),
-                Text(
-                  DateFormat.jm().format(_selectedTimeBegin),
-                  style: const TextStyle(fontSize: 30),
+                GestureDetector(
+                  onTap: () {
+                    _selectTime(true);
+                  },
+                  child: Text(
+                    DateFormat.jm().format(selectedTimeBegin),
+                    style: const TextStyle(fontSize: 30),
+                  ),
                 ),
               ],
             ),
@@ -64,16 +101,26 @@ class _NewTodoState extends State<NewTodo> {
             ),
             Row(
               children: [
-                Text(
-                  DateFormat('E, MMM dd, yyyy').format(_selectedTimeEnd),
-                  style: const TextStyle(fontSize: 30),
+                GestureDetector(
+                  onTap: () {
+                    _selectDate(false);
+                  },
+                  child: Text(
+                    DateFormat('E, MMM dd, yyyy').format(selectedTimeEnd),
+                    style: const TextStyle(fontSize: 30),
+                  ),
                 ),
                 const SizedBox(
                   width: 50,
                 ),
-                Text(
-                  DateFormat.jm().format(_selectedTimeEnd),
-                  style: const TextStyle(fontSize: 30),
+                GestureDetector(
+                  onTap: () {
+                    _selectTime(false);
+                  },
+                  child: Text(
+                    DateFormat.jm().format(selectedTimeEnd),
+                    style: const TextStyle(fontSize: 30),
+                  ),
                 ),
               ],
             ),
