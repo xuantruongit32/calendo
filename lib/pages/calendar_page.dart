6d2d830bac70c172c24c17d2394b074c297f9bc2
Hoang Truong <xuantruongit32@gmail.com>
2024-04-02 07:49:32 +0700

    feat: add calendarTapped and deleteTodo(not done) method

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -11,6 +11,7 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  Appointment? _selectedAppointment;
   void _addNewTodo(Appointment appointment) {
     setState(() {
       Database.todo.add(appointment);
@@ -55,6 +56,14 @@ class _CalendarPageState extends State<CalendarPage> {
     }
   }
 
+  void calendarTapped(CalendarLongPressDetails calendarTapDetails) {
+    if (calendarTapDetails.targetElement == CalendarElement.agenda ||
+        calendarTapDetails.targetElement == CalendarElement.appointment) {
+      final Appointment appointment = calendarTapDetails.appointments![0];
+      _selectedAppointment = appointment;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -82,22 +91,53 @@ class _CalendarPageState extends State<CalendarPage> {
         allowAppointmentResize: true,
         onDragEnd: dragEnd,
         onAppointmentResizeEnd: resizeEnd,
-        dataSource: MeetingDataSource(getTodo()),
+        onLongPress: (CalendarLongPressDetails details) {
+          if (details.targetElement == CalendarElement.appointment) {
+            calendarTapped(details);
+            print(_selectedAppointment!.subject);
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text('Delete Appointment'),
+                content: const Text('Are you sure you want to delete this appointment?'),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      if (_selectedAppointment != null) {
+                        setState(() {
+                          Database.todo.removeAt(Database.todo.indexOf(_selectedAppointment!));
+                          getTodo().notifyListeners(CalendarDataSourceAction.remove, []..add(_selectedAppointment!));
+                        });
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: const Text('Delete'),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
+        dataSource: getTodo(),
       ),
     );
   }
 }
 
-List<Appointment> getTodo() {
+MeetingDataSource getTodo() {
   if (Database.dynamicList.isEmpty) {
-    return Database.todo;
+    return MeetingDataSource(Database.todo);
   } else {
     for (var item in Database.dynamicList) {
       if (item is Appointment) {
         Database.todo.add(item);
       }
     }
-    return Database.todo;
+    return MeetingDataSource(Database.todo);
   }
 }
 
